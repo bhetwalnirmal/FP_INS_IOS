04a2665b74a85ce1a32db84de5e54e974717ee2f
Sabin Regmi <xawbeenregmi@gmail.com>
2022-05-18 19:18:22 -0400

    Location Struct Added
    -- location struct defined as per the requirement

---
+++ FP_INS_IOS/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  FP_INS_IOS
+//
+//  Created by Sabin Regmi on 18/05/2022.
+//
+
+import Foundation
+import UIKit
+
+// defining our location struct
+struct Location {
+    let locationTitle: String
+    let locationLat: Double
+    let locationLong: Double
+    let locationDescription: String
+    let locationImages: [String]
+    
+    // todo: We might need to change the datatype of video once we see how we gonna store and handle video file
+    let locationVideo: String
+}
+
