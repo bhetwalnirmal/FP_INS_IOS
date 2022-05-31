c633f83d0edc6d5caa69052298077b31ad81d02e
Ishara Abeykoon <isharaux@gmail.com>
2022-05-31 17:18:38 -0400

    Design changes

--- FP_INS_IOS/controller/LocationAddViewController.swift
+++ FP_INS_IOS/controller/LocationAddViewController.swift
@@ -22,6 +22,8 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
     @IBOutlet weak var txtCoOrdinate: UITextField!
     @IBOutlet weak var txtDescription: UITextView!
     @IBOutlet weak var lblSelectedVideo: UILabel!
+    @IBOutlet weak var btnAddVideo: UIButton!
+    @IBOutlet weak var btnSave: UIButton!
     
     // values stored in images and videos
     var valImageArr: [UIImage] = []
@@ -31,9 +33,12 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
         showImagePicker()
     }
     @IBAction func btnAddVideo(_ sender: Any) {
+       
         showVideoPicker()
     }
-    
+    override func viewDidLoad() {
+        txtDescription.layer.borderWidth = 1.0
+    }
 
     private func showVideoPicker() {
         var configuration = PHPickerConfiguration()
@@ -80,6 +85,14 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
                 }
              }
             
+            if(!provider.canLoadObject(ofClass: UIImage.self)){
+                self.btnAddVideo.setTitle("In Process...", for: .normal)
+                self.btnAddVideo.isEnabled = false
+                self.btnSave.isEnabled = false
+                self.btnSave.isHidden = true
+            }
+            
+            
             provider.loadFileRepresentation(forTypeIdentifier: "public.movie") { url, error in
                             guard error == nil else{
                                 print("error in video picker")
@@ -90,6 +103,9 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
                                 print("error 2 in video picker")
                                 return
                             }
+                
+                            
+                
                             let myVideoData = NSData(contentsOf: url)!
                             // create a new filename
                             let fileName = "\(Int(Date().timeIntervalSince1970)).\(url.pathExtension)"
@@ -98,12 +114,17 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
                             let docPaths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
                             let documentsDirectory: AnyObject = docPaths[0] as AnyObject
                             let docDataPath = documentsDirectory.appendingPathComponent(fileName) as String
+                           
                 
                             try? myVideoData.write(to: URL(fileURLWithPath: docDataPath), options: [])
                 
                             self.videoSavedURL = fileName
                             print("video added")
-                self.updateVideoUploadFile(fileName: self.videoSavedURL)
+                            self.updateVideoUploadFile(fileName: self.videoSavedURL)
+                            self.btnAddVideo.setTitle("Video Uploaded", for: .normal)
+                            self.btnAddVideo.isEnabled = false
+                            self.btnSave.isEnabled = true
+                            self.btnSave.isHidden = false
                         }
 
         }
@@ -114,8 +135,8 @@ class LocationAddViewController: UIViewController, PHPickerViewControllerDelegat
     @IBAction func btnSave(_ sender: Any) {
         //extract data from fields
         let valLocationTitle: String = txtLocationName.text!
-        let valLocationLat: Double = Double(txtCoOrdinate.text!)!
-        let valLocationLong: Double = Double(txtCoOrdinate.text!)!
+        let valLocationLat: Double = Double(txtCoOrdinate.text!.split(separator: ",")[0])!
+        let valLocationLong: Double = Double(txtCoOrdinate.text!.split(separator: ",")[1])!
         let valLocationDescription: String = txtDescription.text!
         
         
